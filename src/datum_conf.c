155635fc5418d168932a0456c125865da8a2a732
Léo Haf <leo@haf.ovh>
2026-09-10 11:10:12 +0200

    enable the JSON api by default

--- src/datum_conf.c
+++ src/datum_conf.c
@@ -143,7 +143,7 @@ const T_DATUM_CONFIG_ITEM datum_config_options[] = {
 		.example_default = true,
 		.required = false, .ptr = &datum_config.api_modify_conf, 						.default_bool = false },
 	{ .var_type = DATUM_CONF_BOOL, 		.category = "api",			.name = "json_api", 			.description = "Enable the JSON API endpoints",
-		.required = false, .ptr = &datum_config.api_json_api,							.default_bool = false},
+		.required = false, .ptr = &datum_config.api_json_api,							.default_bool = true},
 	
 	// extra block submissions list
 	{ .var_type = DATUM_CONF_STRING_ARRAY, 	.category = "extra_block_submissions", 	.name = "urls",		.description = "Array of bitcoind RPC URLs to submit our blocks to directly.  Include auth info: http://user:pass@IP",
